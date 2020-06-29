1360870409b9cd634fe40eb888b384c549852b3e
YanFang <838641570@qq.com>
2020-06-29 10:06:08 +0800

    无状态组件

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,27 +1,25 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { Input, Button, List } from 'antd';
 
 /**
- * UI组件，主要负责界面的渲染
+ * 无状态组件，比UI组件、容器组件性能高，是UI组件的简写
  */
-class TodoListUI extends Component {
-    render() {
-        return (
-            <Fragment>
-                <div>
-                    <label htmlFor="insertArea">输入内容</label>
-                    <Input placeholder="todo..." style={{width: '300px', marginRight: '10px'}}
-                        onChange={this.props.handleInputChange} value={this.props.inputValue}></Input>
-                    <Button type="primary" onClick={this.props.handleBtnClick}>提交</Button>
-                </div>
-                <List style={{marginTop: '10px',width: '300px'}}
-                    bordered
-                    dataSource={this.props.list}
-                    renderItem={ (item, index) => (<List.Item onClick={() => {this.props.handleItemDelete(index)}}>{item}</List.Item>)}
-                />
-            </Fragment>
-        )
-    }
+const TodoListUI = (props) => {
+    return (
+        <Fragment>
+            <div>
+                <label htmlFor="insertArea">输入内容</label>
+                <Input placeholder="todo..." style={{width: '300px', marginRight: '10px'}}
+                    onChange={props.handleInputChange} value={props.inputValue}></Input>
+                <Button type="primary" onClick={props.handleBtnClick}>提交</Button>
+            </div>
+            <List style={{marginTop: '10px',width: '300px'}}
+                bordered
+                dataSource={props.list}
+                renderItem={ (item, index) => (<List.Item onClick={() => {props.handleItemDelete(index)}}>{item}</List.Item>)}
+            />
+        </Fragment>
+    )
 }
 
 export default TodoListUI;
